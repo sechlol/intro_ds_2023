93a29c9f424e7c57cd02c127e22594a511900c63
julialexax <juliapuk@cubbli18-gold.local>
2023-10-10 21:03:13 +0300

    correlation matrix

--- data_vis/data_exploration.py
+++ data_vis/data_exploration.py
@@ -2,14 +2,16 @@ import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
 
+
 def explore_something(dataset: pd.DataFrame):
     print("Much exploration! Very data! Oh Yeah!")
 
-def correlation_matrix(dataset: pd.DataFrame) -> pd.DataFrame:
-    corr_matrix = dataset.corr()
+
+def correlation_matrix(data: pd.DataFrame) -> pd.DataFrame:
+    corr_matrix = data.corr()
 
     fig, ax = plt.subplots(figsize=(15, 12))
-    cmap = sb.diverging_palette(20, 220, as_cmap=True)
+    cmap = sb.diverging_palette(5, 260, as_cmap=True)
     dataplot = sb.heatmap(corr_matrix,
                           cmap=cmap,
                           annot=True,
