2eef9f23fed836e219b3ccc2a6fa53d1621284f0
sechlol <ccardin.sech@gmail.com>
2023-10-29 13:56:49 +0200

    reformatted main, aggregated visualization functions into a separate file

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
 import data_collection as dc
-import data_vis
-
 import data_vis.data_exploration as de
-import data_vis.minimum_spanning_tree as mst
 import ml.super_duper_ai as ai
 
 _DATASET_PATH = "out/dataset.csv"
@@ -12,10 +9,10 @@ _DATASET_PATH = "out/dataset.csv"
 
 def collect_data():
     data_sources = [
-        dc.CsvDataSource(path=_DATASET_PATH),
-        # dc.YahooDataSource(),
-        # dc.FredDataSource(),
-        # dc.AlphaDataSource(),
+        dc.YahooDataSource(),
+        dc.FredDataSource(),
+        dc.AlphaDataSource(),
+        # dc.CsvDataSource(path=_DATASET_PATH),
     ]
 
     dataset = dc.aggregate_sources(data_sources)
@@ -29,22 +26,16 @@ def read_dataset():
 
 def main():
     # Get data
-    # print("** DATA COLLECTION **")
+    print("** DATA COLLECTION **")
     # dataset = collect_data()
     dataset = read_dataset()
 
     print("\n** DATA EXPLORATION **")
-    de.correlation_matrix(dataset)
-    de.relative_returns(dataset)
-    de.correlation_slider(dataset, 'XLE', 'XLY')
-    mst.minimum_spanning_tree(dataset, '2015-12-1', '2016-12-1')
+    de.explore_data(dataset)
 
     # Run ML models and visualize results
     print("\n** ML MODEL TRAINING **")
-    #model_result = ai.do_something(dataset)
-
-    # print("\n** RESULT VISUALIZATION **")
-    # data_vis.visualize_something(model_result.history)
+    #ai.do_something(dataset)
 
 
 if __name__ == "__main__":
